5635977a05dc20990c7b7ae65eef6bdc69f1d051
Tranfu <tranfu@qq.com>
2017-08-25 11:07:46 +0800

    red heart random animate class name

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,101 @@
 import React from 'react'
 
 export default class Header extends React.Component {
+
+  componentDidMount() {
+    const classNames = [
+      'bounce',
+      'flash',
+      'pulse',
+      'rubberBand',
+      'shake',
+      'headShake',
+      'swing',
+      'tada',
+      'wobble',
+      'jello',
+      'bounceIn',
+      'bounceInDown',
+      'bounceInLeft',
+      'bounceInRight',
+      'bounceInUp',
+      'bounceOut',
+      'bounceOutDown',
+      'bounceOutLeft',
+      'bounceOutRight',
+      'bounceOutUp',
+      'fadeIn',
+      'fadeInDown',
+      'fadeInDownBig',
+      'fadeInLeft',
+      'fadeInLeftBig',
+      'fadeInRight',
+      'fadeInRightBig',
+      'fadeInUp',
+      'fadeInUpBig',
+      'fadeOut',
+      'fadeOutDown',
+      'fadeOutDownBig',
+      'fadeOutLeft',
+      'fadeOutLeftBig',
+      'fadeOutRight',
+      'fadeOutRightBig',
+      'fadeOutUp',
+      'fadeOutUpBig',
+      'flipInX',
+      'flipInY',
+      'flipOutX',
+      'flipOutY',
+      'lightSpeedIn',
+      'lightSpeedOut',
+      'rotateIn',
+      'rotateInDownLeft',
+      'rotateInDownRight',
+      'rotateInUpLeft',
+      'rotateInUpRight',
+      'rotateOut',
+      'rotateOutDownLeft',
+      'rotateOutDownRight',
+      'rotateOutUpLeft',
+      'rotateOutUpRight',
+      'hinge',
+      'jackInTheBox',
+      'rollIn',
+      'rollOut',
+      'zoomIn',
+      'zoomInDown',
+      'zoomInLeft',
+      'zoomInRight',
+      'zoomInUp',
+      'zoomOut',
+      'zoomOutDown',
+      'zoomOutLeft',
+      'zoomOutRight',
+      'zoomOutUp',
+      'slideInDown',
+      'slideInLeft',
+      'slideInRight',
+      'slideInUp',
+      'slideOutDown',
+      'slideOutLeft',
+      'slideOutRight',
+      'slideOutUp',
+    ]
+    setInterval(() => {
+      const random = this.getRandomInt(0, 75)
+      $('#redheart').removeClass().addClass('animated').addClass(classNames[random])
+    }, 1000 * 10);
+  }
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min //The maximum is exclusive and the minimum is inclusive
+  }
+
   render() {
     return (
-      <nav data-header="Header" className="navbar navbar-default">
+      <nav data-header="Header" className="navbar navbar-inverse">
         <div className="container-fluid">
           <div className="navbar-header">
             <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -12,7 +104,9 @@ export default class Header extends React.Component {
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
-            <a className="navbar-brand" href="#">Brand</a>
+            <a className="navbar-brand" href="#" style={{fontSize:'30px'}}>
+              Brand <span className="animated infinite pulse" id="redheart" style={{display:'inline-block',color:'rgb(233, 50, 45)'}}>❤</span>
+            </a>
           </div>
 
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
